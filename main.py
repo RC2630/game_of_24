808a066a4177b17e6274643137e28f85fdb9cc66
Lucas Qin <lucasqin2002edu@gmail.com>
2025-10-03 01:37:45 -0400

    modified driver into a loop

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from game_24 import SearchTree
 
 def run_game_24() -> None:
-    operands_raw: str = input("\nEnter the 4 numbers separated by a space: ")
-    operands: list[int] = [int(operand_raw) for operand_raw in operands_raw.split(" ")]
-    try:
-        solution: str = SearchTree(operands, enable_logging = True).search()
-        print(f"\nA solution was found: {solution}")
-    except RuntimeError:
-        print(f"\nNo solution was found!")
+    while True:
+        operands_raw: str = input("\nEnter the 4 numbers separated by a space (or \"quit\"): ")
+        if operands_raw == "quit":
+            print("Thank you for playing with this program. Have a nice day!")
+            break
+        operands: list[int] = [int(operand_raw) for operand_raw in operands_raw.split(" ")]
+        try:
+            solution: str = SearchTree(operands, enable_logging = True).search()
+            print(f"A solution was found: {solution}")
+        except RuntimeError:
+            print(f"No solution was found!")
 
 if __name__ == "__main__":
     run_game_24()
